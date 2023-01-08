d0410bf19bdeec5fd4b63b5fe53e94431b7d33b6
Hamid SN <h_sedghy@yahoo.com>
2023-01-08 15:25:12 +1100

    Add files via upload

---
+++ app/src/main/java/com/app/facerecognition/callBack/MainCallBack.java
@@ -0,0 +1,14 @@
+package com.app.facerecognition.callBack;
+
+public interface MainCallBack {
+    void onSavedActionClick();
+
+    void onSaveActionClick();
+
+    void onCameraSwitchClick();
+
+    void onRecognizeClick();
+
+    void onAddFaceClick();
+
+}
